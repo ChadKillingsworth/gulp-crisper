afb97ba2d3716cccb04fa7a3847613cac87c30a6
Chad Killingsworth <chadkillingsworth@gmail.com>
2016-11-02 14:49:18 -0500

    Add initial sourcemap support

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var crisper = require('crisper');
 var oassign = require('object-assign');
+var applySourceMap = require('vinyl-sourcemaps-apply');
 
+var sourceMapCommentExpr = /\n\/\/# sourceMappingURL\=data:application\/json;charset=utf8;base64,([a-zA-Z0-9\+\/=]+)\n$/;
 function splitFile(file, filename, contents) {
-  return new gutil.File({
+  var sourceMapContentParts = sourceMapCommentExpr.exec(contents);
+  var vinylFile = new gutil.File({
     cwd: file.cwd,
     base: file.base,
-    path: path.join(path.dirname(file.path), filename),
-    contents: new Buffer(contents)
+    path: path.join(path.dirname(file.path), filename)
   });
+
+  if (sourceMapContentParts !== null) {
+    vinylFile.contents = new Buffer(contents.replace(sourceMapCommentExpr, '').replace(/\s+$/, '').trim());
+    var sourceMapContent = JSON.parse(Buffer.from(sourceMapContentParts[1], 'base64').toString());
+    sourceMapContent.file = vinylFile.relative;
+    vinylFile.sourceMap = sourceMapContent;
+    applySourceMap(vinylFile, sourceMapContent);
+  } else {
+    vinylFile.contents = new Buffer(contents);
+  }
+
+  return vinylFile;
 }
 
 function getFilename(filepath) {
@@ -35,10 +49,11 @@ module.exports = function (opts) {
     }
 
     var splitfile = getFilename(file.path);
-    var split = crisper(oassign({}, {
+    var split = crisper(oassign({}, opts, {
       source: file.contents.toString(),
-      jsFileName: splitfile.js
-    }, opts));
+      jsFileName: splitfile.js,
+      sourcePath: file.relative
+    }));
     var stream = this;
 
     Object.keys(split).forEach(function(type) {
